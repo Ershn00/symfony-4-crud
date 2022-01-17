7276049996a082136343ff7c42e89241e895b4c8
Ersan Ertuzun <wtc.ershan@gmail.com>
2022-01-17 14:54:43 +0300

    Error fixed: Entry of records through CLI command. (php bin/console app:manage-users)

--- src/Command/ManageUsersCommand.php
+++ src/Command/ManageUsersCommand.php
@@ -4,7 +4,6 @@ namespace App\Command;
 
 use App\Entity\Colleague;
 use App\Repository\ColleagueRepository;
-use Doctrine\DBAL\Exception;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -18,8 +17,9 @@ class ManageUsersCommand extends Command
     protected static $defaultName = 'app:manage-users';
     protected static string $defaultDescription = 'Manage colleague information.';
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, $projectDir)
     {
+        $this->projectDir = $projectDir;
         $this->entityManager = $entityManager;
         parent::__construct();
     }
@@ -33,28 +33,36 @@ class ManageUsersCommand extends Command
             ->addArgument('notes', InputArgument::OPTIONAL, 'Enter your notes')
             ->addOption('name', null, InputOption::VALUE_REQUIRED, 'Enter name:')
             ->addOption('email', null, InputOption::VALUE_REQUIRED, 'Enter email:')
-            ->addOption('notes', null, InputOption::VALUE_REQUIRED, 'Enter notes:')
+            ->addOption('notes', null, InputOption::VALUE_OPTIONAL, 'Enter notes:')
         ;
     }
 
 
     /**
-     * @throws Exception
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        //dd($this->projectDir);
         $io = new SymfonyStyle($input, $output);
-        //$arg1 = $input->getArgument('name');
-        //$arg2 = $input->getArgument('email');
-        //$arg3 = $input->getArgument('notes');
 
-        //if ($arg1) {
-        //    $io->note(sprintf('You passed an argument: %s', $arg1));
-        //}
-
+        //List entered data
         list($name, $email, $notes) = $this->validateUser($io);
 
+        /** @var ColleagueRepository $repo */
+        //Test if Repository is working properly...
         //$repo = $this->entityManager->getRepository(Colleague::class);
+        //$e = $repo->findAll(); dd($e);
+
+        //Enter record in the database
+        $newColleague = new Colleague();
+        $newColleague->setName($name);
+        $newColleague->setEmail($email);
+        $newColleague->setNotes($notes);
+        $newColleague->setCreatedAt(new \DateTime('now'));
+        $this->entityManager->persist($newColleague);
+        $this->entityManager->flush();
+
+        //dd($newColleague);
 
         $io->success('Success! You have saved the information for '.$name.','.$email.','.$notes.' in the database.');
 
@@ -90,16 +98,6 @@ class ManageUsersCommand extends Command
                 $email = $io->ask('Enter Email:');
             }
         $notes = $io->ask('Enter Notes:');
-
-            /** @var ColleagueRepository $repo */
-        $repo = $this->entityManager->getRepository(Colleague::class);
-        $newColleague = new Colleague();
-        $newColleague->setName($name);
-        $newColleague->setEmail($email);
-        $newColleague->setNotes($notes);
-        $newColleague->setPicture('');
-        $newColleague->setCreatedAt(new \DateTime('now'));
-        $this->entityManager->persist($repo);
 
         return array($name, $email, $notes);
     }
