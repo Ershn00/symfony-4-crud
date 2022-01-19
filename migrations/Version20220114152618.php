b331e23bf2bd8da714486c0b0da238fb002bcd66
Ersan Ertuzun <wtc.ershan@gmail.com>
2022-01-19 18:13:57 +0300

    Reviewed the requirements document and added missing items. Also, I added a functional test for email sending. I disabled email delivery per instructions in the document.

--- migrations/Version20220114152618.php
+++ migrations/Version20220114152618.php
@@ -20,7 +20,7 @@ final class Version20220114152618 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE colleagues (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, notes CLOB NOT NULL, created_at DATETIME NOT NULL)');
+        $this->addSql('CREATE TABLE colleagues (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, image VARCHAR(255) NULL, notes CLOB NULL, created_at DATETIME NULL)');
     }
 
     public function down(Schema $schema): void
